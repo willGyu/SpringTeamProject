fbc48b1c0711028c1dba75c18c5e03999315e2dc
dahyunzip <bydhyun@naver.com>
2025-10-14 13:00:45 +0900

    테스트.. 빨줄 지우기..

--- SpringTeamProject/src/main/java/com/itwillbs/controller/SampleController.java
+++ SpringTeamProject/src/main/java/com/itwillbs/controller/SampleController.java
@@ -2,36 +2,36 @@ package com.itwillbs.controller;
 
 import java.lang.System.Logger;
 
-import org.slf4j.LoggerFactory;
+//import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import lombok.extern.java.Log;
-import lombok.extern.log4j.Log4j;
+//import lombok.extern.java.Log;
+//import lombok.extern.log4j.Log4j;
 
-@Log4j
+
 @RequestMapping("/sample/*")
 @Controller
 public class SampleController {
 
     
-	
-
+	//private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
 
     
 	@GetMapping("/all")
 	public void doAll() {
-		log.info("do all access everybody");
+		//logger.info("do all access everybody");
 	}
 	
 	@GetMapping("/member")
 	public void doMember() {
-		log.info("logined member");
+		//logger.info("logined member");
 	}
 	
-	@GetMapping("/admin"){
-		log.info("admin only");
+	@GetMapping("/admin")
+	public void doB() {
+		//logger.info("admin only");
 	}
 	
 }
